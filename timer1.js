2a294aefda43014594cee332c012f622c377063c
KurtSpiker <kurt.spiker@gmail.com>
2021-09-21 20:20:40 -0600

    Add files via upload

---
+++ timer1.js
@@ -0,0 +1,25 @@
+var args = process.argv.slice(2);
+if(args.length <= 0) {
+  console.log("Enter alarm times");
+}
+
+let elementArray = [];
+for (let element of args) {
+  if ( typeof Number(element) === "number" && Number(element) > 0) {
+    elementArray.push(element);
+  }
+}
+
+let numArray = elementArray.sort(function(a, b) {
+  return a - b;
+});
+
+const alarm = (someArray) => {
+  for (let num of elementArray) {
+    setTimeout(() => {
+      process.stdout.write('.');
+    }, num * 1000);
+  }
+}
+
+alarm(numArray);
